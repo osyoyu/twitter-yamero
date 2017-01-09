9e73c7a660769f82ea387e1d6f501ace7889fbb2
Daisuke Aritomo <di.aritomo@gmail.com>
2017-01-09 17:40:27 +0900

    みんなありがとう
    https://twitter.com/KOBA789/status/818374617249169408
    https://twitter.com/hnle0/status/818375289080201216
    https://twitter.com/KOBA789/status/818375792014938112(

--- background.js
+++ background.js
@@ -1,33 +1,26 @@
 "use strict";
 
-console.log('hogefugapiyo');
-
-chrome.tabs.onUpdated.addListener(function (tabId) {
+chrome.tabs.onUpdated.addListener((tabId) => {
   // populate all tabs
-  let tabs = [];
+  const tabs = [];
   chrome.windows.getAll({ populate: true }, function (windows) {
     windows.forEach((window) => {
       window.tabs.forEach((tab) => {
         tabs.push(tab);
       });
     });
-  });
 
-  setTimeout(() => {
     // count twitter.com tabs
     let twitterTabsCount = 0;
     tabs.forEach((tab) => {
-      const parser = document.createElement('a');
-      parser.href = tab.url;
-      console.log(parser.hostname);
-      if (parser.hostname === 'twitter.com') { twitterTabsCount += 1; }
+      const url = new URL(tab.url);
+      if (url.hostname === 'twitter.com') { twitterTabsCount += 1; }
     });
-
-    console.log(twitterTabsCount);
 
     if (twitterTabsCount > 2) {
       alert('Twitter やめろ');
       chrome.tabs.remove(tabId);
     }
-  }, 100);
+  });
+
 });
