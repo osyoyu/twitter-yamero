427dca7075c11f2c275a87814dba40255ede1913
Daisuke Aritomo <di.aritomo@gmail.com>
2017-01-17 23:01:20 +0900

    Fire only on URL changes

--- background.js
+++ background.js
@@ -1,26 +1,38 @@
 "use strict";
 
-chrome.tabs.onUpdated.addListener((tabId) => {
-  // populate all tabs
-  const tabs = [];
-  chrome.windows.getAll({ populate: true }, function (windows) {
-    windows.forEach((window) => {
-      window.tabs.forEach((tab) => {
-        tabs.push(tab);
+chrome.browserAction.setBadgeText({
+  text: "ON"
+})
+
+chrome.browserAction.onClicked.addListener((tab) => {
+});
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
+  if (changeInfo.hasOwnProperty('url')) {
+    // populate all tabs
+    const tabs = [];
+    chrome.windows.getAll({ populate: true }, function (windows) {
+      windows.forEach((window) => {
+        window.tabs.forEach((tab) => {
+          tabs.push(tab);
+        });
       });
+
+      // count twitter.com tabs
+      let twitterTabsCount = 0;
+      tabs.forEach((tab) => {
+        const url = new URL(tab.url);
+        if (url.hostname === 'twitter.com') { twitterTabsCount += 1; }
+      });
+
+      chrome.browserAction.setBadgeText({
+        text: twitterTabsCount.toString()
+      });
+
+      if (twitterTabsCount > 2) {
+        alert('Twitter やめろ');
+        chrome.tabs.remove(tabId);
+      }
     });
-
-    // count twitter.com tabs
-    let twitterTabsCount = 0;
-    tabs.forEach((tab) => {
-      const url = new URL(tab.url);
-      if (url.hostname === 'twitter.com') { twitterTabsCount += 1; }
-    });
-
-    if (twitterTabsCount > 2) {
-      alert('Twitter やめろ');
-      chrome.tabs.remove(tabId);
-    }
-  });
-
+  }
 });
