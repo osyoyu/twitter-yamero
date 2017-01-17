a3d2ffda4739a4d9ac037215d56b683b55a48cf4
Daisuke Aritomo <di.aritomo@gmail.com>
2017-01-18 00:30:13 +0900

    Make extension icon toggleable

--- background.js
+++ background.js
@@ -1,38 +1,66 @@
 "use strict";
 
-chrome.browserAction.setBadgeText({
-  text: "ON"
-})
+let active = false;
 
-chrome.browserAction.onClicked.addListener((tab) => {
-});
+function countTwitterTabs(callback) {
+  let twitterTabsCount = 0;
+
+  chrome.tabs.query({}, (tabs) => {
+    tabs.forEach((tab) => {
+      const url = new URL(tab.url);
+      if (url.hostname === 'twitter.com') { twitterTabsCount += 1; }
+    });
+
+    callback(twitterTabsCount);
+  });
+}
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
   if (changeInfo.hasOwnProperty('url')) {
-    // populate all tabs
-    const tabs = [];
-    chrome.windows.getAll({ populate: true }, function (windows) {
-      windows.forEach((window) => {
-        window.tabs.forEach((tab) => {
-          tabs.push(tab);
-        });
-      });
-
-      // count twitter.com tabs
-      let twitterTabsCount = 0;
-      tabs.forEach((tab) => {
-        const url = new URL(tab.url);
-        if (url.hostname === 'twitter.com') { twitterTabsCount += 1; }
-      });
-
+    countTwitterTabs((count) => {
       chrome.browserAction.setBadgeText({
-        text: twitterTabsCount.toString()
+        text: count.toString()
       });
 
-      if (twitterTabsCount > 2) {
+      if (active && count > 2) {
         alert('Twitter やめろ');
         chrome.tabs.remove(tabId);
       }
+    });
+  }
+});
+
+chrome.tabs.onRemoved.addListener(() => {
+  // update badge
+  countTwitterTabs((count) => {
+    chrome.browserAction.setBadgeText({
+      text: count.toString()
+    });
+  });
+});
+
+chrome.browserAction.onClicked.addListener((tab) => {
+  if (active) {
+    active = false;
+    chrome.browserAction.setIcon({
+      path: {
+        '16': 'icons/icon16-inactive.png'
+      }
+    });
+  }
+  else {
+    countTwitterTabs((count) => {
+      if (count > 2) {
+        alert("まず Twitter をやめる意志を見せろ");
+        return;
+      }
+
+      active = true;
+      chrome.browserAction.setIcon({
+        path: {
+          '16': 'icons/icon16-active.png'
+        }
+      });
     });
   }
 });
