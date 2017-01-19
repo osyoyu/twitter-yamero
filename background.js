7c54d3919bad53cbe066cc5418a743665cf43414
Hidekazu Kobayashi <kobahide789@gmail.com>
2017-01-19 23:14:30 +0900

    SUPER OK

--- background.js
+++ background.js
@@ -1,66 +1,110 @@
 "use strict";
 
-let active = true;
+/* global chrome */
 
-function countTwitterTabs(callback) {
-  let twitterTabsCount = 0;
+const MAX_TWITTER_TABS = 2;
 
-  chrome.tabs.query({}, (tabs) => {
-    tabs.forEach((tab) => {
-      const url = new URL(tab.url);
-      if (url.hostname === 'twitter.com') { twitterTabsCount += 1; }
-    });
+class App {
+  constructor() {
+    this.isActive = false;
+    this.twitterTabs = new Map([]);
 
-    callback(twitterTabsCount);
-  });
-}
+    const handlerNames = Object.getOwnPropertyNames(App.prototype).filter(key => key.match(/^on[A-Z]/) !== null);
+    for (const handlerName of handlerNames) {
+      this[handlerName] = this[handlerName].bind(this);
+    }
 
-chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
-  if (changeInfo.hasOwnProperty('url')) {
-    countTwitterTabs((count) => {
-      chrome.browserAction.setBadgeText({
-        text: count.toString()
-      });
+    chrome.browserAction.onClicked.addListener(this.onButtonClicked);
+    chrome.tabs.onUpdated.addListener(this.onTabUpdate);
+    chrome.tabs.onRemoved.addListener(this.onTabRemove);
 
-      if (active && count > 2) {
-        alert('Twitter やめろ');
-        chrome.tabs.remove(tabId);
-      }
+    this.collectTabsStats();
+  }
+
+  collectTabsStats() {
+    chrome.tabs.query({
+      url: '*://twitter.com/*'
+    }, (tabs) => {
+      this.twitterTabs = new Map(tabs.map(tab => [tab.id, tab]));
+
+      this.onTwitterTabsUpdate();
     });
   }
-});
 
-chrome.tabs.onRemoved.addListener(() => {
-  // update badge
-  countTwitterTabs((count) => {
+  onTwitterTabsUpdate() {
+    this.updateBadge();
+  }
+
+  updateBadge() {
     chrome.browserAction.setBadgeText({
-      text: count.toString()
+      text: this.twitterTabs.size.toString()
     });
-  });
-});
+  }
 
-chrome.browserAction.onClicked.addListener((tab) => {
-  if (active) {
-    active = false;
+  onTabUpdate(tabId, changeInfo, tab) {
+    if (Object.hasOwnProperty.call(changeInfo, 'url')) {
+      const urlObj = new URL(changeInfo.url);
+      if (urlObj.hostname === 'twitter.com') {
+        this.onNewTwitterTab(tab);
+      }
+    }
+  }
+
+  onNewTwitterTab(tab) {
+    this.twitterTabs.set(tab.id, tab);
+    this.onTwitterTabsUpdate();
+    this.twitterYamero(tab);
+  }
+
+  onTabRemove(tabId) {
+    this.twitterTabs.delete(tabId);
+    this.onTwitterTabsUpdate();
+  }
+
+  twitterYamero(tab) {
+    if (!this.isActive) { return; }
+    if (!this.isTooManyTwitterTabs()) { return; }
+
+    chrome.tabs.remove(tab.id);
+
+    this.onTabRemove(tab.id);
+    chrome.tabs.highlight({ tabs: [...this.twitterTabs.values()].map(tab => tab.index) });
+  }
+
+  onButtonClicked() {
+    if (this.isActive) {
+      this.disableYamero();
+    } else {
+      this.enableYamero();
+    }
+  }
+
+  changeIcon(iconPath) {
     chrome.browserAction.setIcon({
       path: {
-        '16': 'icons/icon16-inactive.png'
+        '16': iconPath
       }
     });
   }
-  else {
-    countTwitterTabs((count) => {
-      if (count > 2) {
-        alert("まず Twitter をやめる意志を見せろ");
-        return;
-      }
 
-      active = true;
-      chrome.browserAction.setIcon({
-        path: {
-          '16': 'icons/icon16.png'
-        }
-      });
-    });
+  enableYamero() {
+    if (this.isTooManyTwitterTabs()) {
+      alert("まず Twitter をやめる意志を見せろ");
+      return;
+    }
+
+    this.isActive = true;
+    this.changeIcon('icons/icon16-inactive.png');
   }
-});
+
+  disableYamero() {
+    this.isActive = false;
+    this.changeIcon('icons/icon16.png');
+  }
+
+  isTooManyTwitterTabs() {
+    return this.twitterTabs.size > MAX_TWITTER_TABS;
+  }
+}
+
+new App();
